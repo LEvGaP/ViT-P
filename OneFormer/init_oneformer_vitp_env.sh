6047f5b37c248f3178e6c4402a949934548a7d82
Evgenii Lapshin <zhenia.lapshin201@yandex.ru>
2025-12-28 11:20:38 +0500

    oneformer installation scripts

---
+++ OneFormer/init_oneformer_vitp_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+module load gpu/cuda-11.6
+
+source /opt/conda/etc/profile.d/conda.sh
+conda create --prefix env python=3.12 -y
+
+conda activate ./env
+
+pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu118
+
+pip3 install numpy==1.26.4 opencv-python==4.10.0.82
+
+python tools/setup_detectron2.py
+
+pip3 install 'pip<24.1'
+
+pip3 install -r requirements.txt
+
+cd oneformer/modeling/pixel_decoder/ops
+sh custom_make.sh
+cd ../../../..
+
+cd oneformer/modeling/backbone/ops_dcnv3
+sh custom_make.sh
+sh run_test.sh
+cd ../../../..
+
+cd detectron2
+pip install -e .
+cd ..
---
+++ OneFormer/oneformer/modeling/backbone/ops_dcnv3/run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load gpu/cuda-11.6
+
+GPUS=1
+NODE_PARAMS="-p hiperf --gres=gpu:a100:$GPUS --nodelist=tesla-a101 -t 00:15:00"
+
+srun -n1 \
+    --cpus-per-task=8 \
+    --mem=45000 \
+    $NODE_PARAMS \
+    --job-name=vit-p \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS} \
+    python test.py
